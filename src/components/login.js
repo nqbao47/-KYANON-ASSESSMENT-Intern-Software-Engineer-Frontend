58d34e24c331619afac5ac62b1633858cb343943
nqbao47 <baonguyengau14@gmail.com>
2023-03-24 22:12:05 +0700

    Add validate for input

--- src/components/login.js
+++ src/components/login.js
@@ -1,39 +1,53 @@
-import React, { useState } from 'react';
-import "./login.css"; 
+import React, { useState } from "react";
+import "./login.css";
 
 export default function Login() {
   const [showPassword, setShowPassword] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
 
-    function handleCheckbox() {
-        setShowPassword(!showPassword);
-    };
+  function handleCheckbox() {
+    setShowPassword(!showPassword);
+  }
+
+  function handleLogin(event) {
+    event.preventDefault();
+    setIsSuccess(true);
+  }
 
   return (
-    <div className='login-page'>
-      <div className='login-page-container'>
+    <div className="login-page">
+      <div className="login-page-container">
         <h1>Login</h1>
-        <form>
+        <form onSubmit={handleLogin}>
           <div>
-            <label htmlFor='email' className='form-label'>
+            <label htmlFor="email" className="form-label">
               Email
             </label>
-            <input id='email' className='form-control' type='text' name='firstname'></input>
+            <input
+              id="email"
+              className="form-control"
+              type="email"
+              name="email"
+              required
+            ></input>
           </div>
-          <div className='div'>
-            <label htmlFor='password' className='form-label'>
+
+          <div className="div">
+            <label htmlFor="password" className="form-label">
               Password
             </label>
             <div className="password-container">
               <input
-                id='password'
-                className='form-control'
-                type={showPassword ? 'text' : 'password'} 
-                name='password'
+                id="password"
+                className="form-control"
+                type={showPassword ? "text" : "password"}
+                name="password"
+                required
               />
-              <label htmlFor="show-password-checkbox" className='checkBox'>
+              <label htmlFor="show-checkbox" className="checkBox">
                 <input
-                  id="show-password-checkbox"
-                  className='checkBox'
+                  id="show-checkbox"
+                  className="checkBox"
                   type="checkbox"
                   onClick={handleCheckbox}
                 />
@@ -41,9 +55,10 @@ export default function Login() {
               </label>
             </div>
           </div>
-          <button type='submit' className='btn-submit'>
+          <button type="submit" className="btn-submit">
             Sign in
           </button>
+          {isSuccess && <div className="alert">Signed in successfully !</div>}
         </form>
       </div>
     </div>
